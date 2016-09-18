4ca9d327cdd238a3a963bf73f5b463a31c70a5a5
Danny Tang <data1013@mit.edu>
2016-09-18 00:44:13 -0400

    implemented quad bowl and its derivative

--- hw1/code/P1/q1.py
+++ hw1/code/P1/q1.py
@@ -1,4 +1,6 @@
 import numpy as np
+from loadParametersP1 import getData as getParameters
+from loadFittingDataP1 import getData as getFittingData
 
 """
     Runs gradient descent given an objective function along
@@ -13,3 +15,18 @@ def gradientDescent(obj_func, grad_func, init, step, epsilon):
         previous_value = current_value
         current_value -= step * grad_func(current_value)
     return current_value
+
+"""
+Implementation of the Quadratic Bowl function
+"""
+def quadBowl(A, b, x):
+    x_transpose = np.transpose(x)
+    first_term = 0.5 * np.dot(np.dot(x_transpose, A), x)
+    second_term = x_transpose * b
+    return first_term - second_term
+
+"""
+Implementation of the derivative of the Quadratic Bowl function
+"""
+def quadBowlDerivative(A, b, x):
+    return np.dot(A, x) - b
