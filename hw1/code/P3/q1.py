17f0c9b332d819a25dd77e9a3dfbc6b0ac684ffa
Danny Tang <data1013@mit.edu>
2016-09-24 17:16:58 -0400

    implemented q1 problem 3

--- hw1/code/P3/q1.py
+++ hw1/code/P3/q1.py
@@ -1,23 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np 
+import sys
+sys.path.insert(0, '../P2')
 from loadFittingDataP2 import getData as getCurveData
-"""
-y = np.ones(3)
-x = np.polynomial.Polynomial(y)
-"""
+
 """
 	Given an array of 1D points X, a vector of corresponding values Y,
-	and the maximum order of a simple polynomial basis M, returns the
-	maximum likelihood weight vector W_ml
+	the maximum order of a simple polynomial basis M, and a regression
+	term lambda, returns the weight vector w
 """
-def weightML(X, Y, M):
+def ridgeWeights(X, Y, M, lambda_):
 	num_terms = M + 1
 	X_tiled = np.tile(X, (num_terms, 1))
 	phi = X_tiled.T
 	phi = np.power(phi, np.arange(num_terms))
 	phi_transposed = phi.T
 
-	pseudo_inverse = np.dot(np.linalg.inv(np.dot(phi_transposed, phi)), phi_transposed)
+	phi_squared = np.dot(phi_transposed, phi)
+	reg_identity = lambda_ * np.eye(np.shape(phi_squared)[0])
+
+	pseudo_inverse = np.dot(np.linalg.inv(reg_identity - phi_squared), phi_transposed)
 	return np.dot(pseudo_inverse, Y)
 
 M_possible = list(range(1, 11))
@@ -26,7 +28,7 @@ X, Y = getCurveData(False)
 
 def plotGraphs():
 	for M in M_possible:
-		weight_ml = weightML(X, Y, M)
+		weight_ml = ridgeWeights(X, Y, M, 0.01)
 		basis_function = np.polynomial.Polynomial(weight_ml)
 		predicted_Y = np.apply_along_axis(basis_function, 0, X)
 
@@ -34,3 +36,5 @@ def plotGraphs():
 		plt.xlabel('x')
 		plt.ylabel('y')
 		plt.show()
+
+plotGraphs()
