0f91436f4d7317adf34cdad5c0c9356ca39e6f83
data1013 <dannytang1013@gmail.com>
2016-10-25 02:57:01 -0400

    problem 2 -danny

--- hw2/p2.py
+++ hw2/p2.py
@@ -1,7 +1,7 @@
 # SVMs
 
 import numpy as np
-# from cvxopt import matrix, solvers
+from cvxopt import matrix, solvers
 
 def trainAlphas(X, Y, C, kernel=lambda i, j: np.dot(i, j)):
     N = X.shape[0]
@@ -11,35 +11,37 @@ def trainAlphas(X, Y, C, kernel=lambda i, j: np.dot(i, j)):
 
     vectorized_createP = np.vectorize(createP)
 
-    P = matrix(np.fromfunction(vectorized_createP, (N, N)))
-    q = matrix(np.ones(N) * -1.0)
-    G = matrix(np.concatenate((np.diag(np.ones(N)) * -1.0, np.diag(np.ones(N)))))
-    h = matrix(np.concatenate((np.zeros(N), np.ones(N) * C * 1.0)))
-    A = matrix(np.copy(Y))
-    b = 0
+    P = matrix(np.fromfunction(vectorized_createP, (N, N)), tc='d')
+    q = matrix(np.ones(N) * -1.0, tc='d')
+    G = matrix(np.concatenate((np.diag(np.ones(N)) * -1.0, np.diag(np.ones(N)))), tc='d')
+    h = matrix(np.concatenate((np.zeros(N), np.ones(N) * C * 1.0)), tc='d')
+    A = matrix(np.copy(Y).T, tc='d')
+    b = matrix(np.array([[0.0]]), tc='d')
 
     model = solvers.qp(P, q, G, h, A, b)
 
     alphas = np.array(np.copy(model['x']))
+    alphas_T = alphas.T[0]
     alphas_indices = np.indices((N, ))[0]
+    alphas_threshold = 1e-6
 
-    sv_indices = alphas_indices[(alphas > 0.0 and alphas < C)]
+    sv_indices = alphas_indices[np.where(np.logical_and(alphas_T > alphas_threshold, alphas_T < C))]
     num_sv = sv_indices.shape[0] * 1.0
 
     def solveB(idx):
         true_y = Y[idx]
         sv_indices_inner = np.copy(sv_indices)
-        inner_sum = np.apply_along_axis(lambda j: alphas[j] * Y[j] * kernel(X[idx], X[j]), 0, sv_indices_inner)
+        inner_sum = np.apply_along_axis(np.vectorize(lambda j: alphas[j] * Y[j] * kernel(X[idx], X[j])), 0, sv_indices_inner)
         return true_y - np.sum(inner_sum)
 
-    b = np.sum(np.apply_along_axis(solveB, 0, sv_indices)) / num_sv
+    b = np.sum(np.apply_along_axis(np.vectorize(solveB), 0, sv_indices)) / num_sv
 
     return (model, b)
 
-def gaussianRBF(variance):
+def gaussianRBF(bandwidth):
     def gaussianInstance(x, x_prime):
         norm_squared = np.linalg.norm(x - x_prime) ** 2.0
-        var_coeff = -1.0 / (2.0 * variance)
+        var_coeff = -1.0 / (2.0 * bandwidth ** 2)
         return np.exp(norm_squared * var_coeff)
     return gaussianInstance
 
@@ -53,9 +55,10 @@ if __name__ == "__main__":
     primal_obj = model['primal objective']
 
     print alphas
+    print b
     print primal_obj
 
 # test = np.array([1, 4, 5, 20, 3, 1, 54, 6, 45, 10])
 # test_indices = np.indices((test.shape[0], ))[0]
 
-# print test_indices[(test == 4)]
+# print test_indices[np.where(np.logical_and(test > 3, test < 20))]
